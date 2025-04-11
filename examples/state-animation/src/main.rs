3ce3b5065ed767911a96a7502440260c7327e235
tomara_x <414d59@gmail.com>
2025-04-11 22:20:26 +0200

    edit comment

--- examples/state-animation/src/main.rs
+++ examples/state-animation/src/main.rs
@@ -36,10 +36,13 @@ fn setup(
 
             // you can insert in an observer, i'm just testing here
             UiLayoutAnimation::new(vec![
-                // window is a tuple struct, we want the 0 field, then the pos field, that's a UiValue<Vec2>
-                // we want the rl field (that's an option<Vec2>, i know it's Some so i take the 0
-                // field of that, now this is a Vec2 i want the x field
-                // that
+                // will get the 'base' layout from the UiLayout map, which is a UiLayoutType.
+                // everything after the space is a bevy_reflect path
+                // i know the variant is Window which is a tuple struct, we want the 0 field (UiLayoutTypeWindow),
+                // then the pos field of that, that's a UiValue<Vec2>,
+                // we want the rl field (that's an Option<Vec2>),
+                // i know it's Some so i take the 0 field of that, (Vec2)
+                // i want the x field of that
                 ("base .0.pos.rl.0.x", Anim::segs(vec![Seg::To(50., 50.), Seg::Hold(1.), Seg::To(0., 50.), Seg::Hold(1.)]).looping(true)),
                 ("base .0.pos.rl.0.y", Anim::segs(vec![Seg::Hold(1.), Seg::To(50., 50.), Seg::Hold(1.), Seg::To(0., 50.,)]).looping(true).with_end_trig()),
                 // rh, cause that's what i'm inserting below
